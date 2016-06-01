40c3a6019742468d507b757555ba7ed6213e11c5
oloomi <smh.oloomi@gmail.com>
2016-06-01 18:46:20 +1000

    Number of reads not mapped to any location is counted. All numbers are now divided by total number of reads, rather than number of mapped reads.

--- multi-mapping-stats.py
+++ multi-mapping-stats.py
@@ -2,39 +2,44 @@ __author__ = 'soloomi'
 
 from collections import defaultdict
 
-def cigar_counts_stats(sam_file_path, organism):
-    # a nested dictionary like: {read_id: {CIGAR: [list of mapping locations]}
-    read_cigar_dict = defaultdict(lambda: defaultdict(int))
 
-    sam_file = open(sam_file_path)
-    for line in sam_file.readlines():
-        # skip header lines
-        if line[0] == "@":
-            continue
-        fields = line.rstrip("\n").split("\t")
-        read_id = fields[0]
-        cigar = fields[5]
-        # * means no alignment for a read
-        if cigar != "*":
-            # how many times a CIGAR string is seen for each read?
-            # store the mapping locations for each CIGAR (ie. alignment) in a list
-            read_cigar_dict[read_id][cigar].append(1)
+def multi_mapping_stats(sam_file_path, output_file_path):
+    # A nested dictionary like: {read_id: {CIGAR: [list of mapping positions]}
+    read_cigar_dict = defaultdict(lambda: defaultdict(list))
+    not_mapped_reads = 0  # Number of reads that are not mapped to any location
 
-    # total number of reads
-    total_reads = len(read_cigar_dict)
-    # number of reads that map only to one location
+    with open(sam_file_path) as sam_file:
+        for line in sam_file:
+            # Skip header lines
+            if line[0] == "@":
+                continue
+            fields = line.rstrip().split("\t")
+            read_id = fields[0]  # QNAME: Query template NAME
+            cigar = fields[5]  # CIGAR string (ie. alignment)
+            pos = int(fields[3])  # 1-based leftmost mapping POSition
+            # * means no alignment for a read
+            if cigar != "*":
+                # How many times a CIGAR string is seen for each read?
+                # Store the mapping locations for each CIGAR in a list
+                read_cigar_dict[read_id][cigar].append(pos)
+            else:
+                not_mapped_reads += 1
+
+    # Total number of mapped reads
+    total_mapped_reads = len(read_cigar_dict)
+    # Number of reads that map only to one location
     unique_reads = 0
-    # number of reads that map to different locations with the same alignment
+    # Number of reads that map to different locations with the same alignment
     same_multi_reads = 0
-    # number of reads that map to different locations with different alignments
+    # Number of reads that map to different locations with different alignments
     diff_multi_reads = 0
 
-    # for each read
-    for read_id, cigar_counts in read_cigar_dict.items():
+    # For each read
+    for read_id, cigar_dict in read_cigar_dict.items():
         # if this read has only one CIGAR
-        if len(cigar_counts) == 1:
+        if len(cigar_dict) == 1:
             # if it has repeated only once, that's a read which maps only to one location
-            if sum(cigar_counts.values()) == 1:
+            if len(cigar_dict.values()) == 1:
                 unique_reads += 1
             # Otherwise, it maps to more than one location, but with the same alignment
             else:
@@ -42,18 +47,25 @@ def cigar_counts_stats(sam_file_path, organism):
         else:
             diff_multi_reads += 1
 
-    cigar_counts_stats_file = open("stats-cigar-counts-{}.txt".format(organism), "w")
-    cigar_counts_stats_file.write("#reads that map to only one location:\t{}\t{}\n".
-                                  format(unique_reads, round(unique_reads/total_reads, 3)))
-    cigar_counts_stats_file.write("#reads that map more than one location, with same CIGAR:\t{}\t{}\n".
-                                  format(same_multi_reads, round(same_multi_reads/total_reads, 3)))
-    cigar_counts_stats_file.write("#reads that map more than one location, with different CIGAR's:\t{}\t{}\n".
-                                  format(diff_multi_reads, round(diff_multi_reads/total_reads, 3)))
-    cigar_counts_stats_file.write("#all reads:\t{}\n".format(total_reads))
+    # Total number of reads
+    total_reads = total_mapped_reads + not_mapped_reads
 
-    cigar_counts_stats_file.close()
-    sam_file.close()
+    stats_file = open(output_file_path, 'w')
+    stats_file.write('Number of reads that mapped to: \n')
+    stats_file.write('- Only one location:\t{}\t{:.2%}\n'.format(unique_reads, unique_reads / total_reads, 3))
+    stats_file.write('- More than one location, with the same alignment:'
+                     '\t{}\t{:.2%}\n'.format(same_multi_reads, same_multi_reads / total_reads))
+    stats_file.write('- More than one location, with different alignments:'
+                     '\t{}\t{:.2%}\n'.format(diff_multi_reads, diff_multi_reads / total_reads))
+    stats_file.write('Number of reads that did not map to any location:\t{}\t{:.2%}\n'.format(not_mapped_reads,
+                                                                                        not_mapped_reads / total_reads))
+    stats_file.write('Total number of reads:\t{}\t{:.2%}\n'.format(total_reads, total_reads / total_reads))
+    stats_file.write('Total number of mapped reads:\t{}\t{:.2%}\n'.format(total_mapped_reads,
+                                                                          total_mapped_reads / total_reads))
 
-cigar_counts_stats("head-mtb-mapping.sam", "mtb-test")
+    stats_file.close()
+
+
+multi_mapping_stats("head-mtb-mapping.sam", "mtb-test.txt")
 # cigar_counts_stats("mtb-single-mapping-report-all.sam", "mtb-h37rv-single")
 # cigar_counts_stats("ot-single-mapping-report-all.sam", "ot-ikeda-single")
