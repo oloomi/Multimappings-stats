52ecc71f21f1ffe267a492e44443ffb71f373de7
oloomi <smh.oloomi@gmail.com>
2016-06-02 16:52:03 +1000

    Command line argument handling is now added.

--- multi-mapping-stats.py
+++ multi-mapping-stats.py
@@ -1,6 +1,7 @@
 __author__ = 'soloomi'
 
 from collections import defaultdict
+import argparse
 
 
 def multi_mapping_stats(sam_file_path, output_file_path):
@@ -50,22 +51,30 @@ def multi_mapping_stats(sam_file_path, output_file_path):
     # Total number of reads
     total_reads = total_mapped_reads + not_mapped_reads
 
-    stats_file = open(output_file_path, 'w')
-    stats_file.write('Number of reads that mapped to: \n')
-    stats_file.write('- Only one location:\t{}\t{:.2%}\n'.format(unique_reads, unique_reads / total_reads, 3))
-    stats_file.write('- More than one location, with the same alignment:'
-                     '\t{}\t{:.2%}\n'.format(same_multi_reads, same_multi_reads / total_reads))
-    stats_file.write('- More than one location, with different alignments:'
-                     '\t{}\t{:.2%}\n'.format(diff_multi_reads, diff_multi_reads / total_reads))
-    stats_file.write('Number of reads that did not map to any location:\t{}\t{:.2%}\n'.format(not_mapped_reads,
-                                                                                        not_mapped_reads / total_reads))
-    stats_file.write('Total number of reads:\t{}\t{:.2%}\n'.format(total_reads, total_reads / total_reads))
-    stats_file.write('Total number of mapped reads:\t{}\t{:.2%}\n'.format(total_mapped_reads,
-                                                                          total_mapped_reads / total_reads))
+    output_str = ""
+    output_str += 'Number of reads that mapped to: \n'
+    output_str += '- Only one location:\t{}\t{:.2%}\n'.format(unique_reads, unique_reads / total_reads, 3)
+    output_str += '- More than one location, with the same alignment:' + \
+                  '\t{}\t{:.2%}\n'.format(same_multi_reads, same_multi_reads / total_reads)
+    output_str += '- More than one location, with different alignments:' + \
+                  '\t{}\t{:.2%}\n'.format(diff_multi_reads, diff_multi_reads / total_reads)
+    output_str += 'Number of reads that did not map to any location:\t{}\t{:.2%}\n'.format(not_mapped_reads,\
+                                                                                           not_mapped_reads / total_reads)
+    output_str += 'Total number of reads:\t{}\t{:.2%}\n'.format(total_reads, total_reads / total_reads)
+    output_str += 'Total number of mapped reads:\t{}\t{:.2%}\n'.format(total_mapped_reads,\
+                                                                       total_mapped_reads / total_reads)
 
-    stats_file.close()
+    with open(output_file_path, 'w') as stats_file:
+        stats_file.write(output_str)
+    print(output_str)
 
+# Getting command line arguments
+parser = argparse.ArgumentParser('Finds multi-mapping statistics')
+parser.add_argument('-i', '--input', required=True, help='The input SAM file that contains read mappings')
+parser.add_argument('-o', '--output', default='output.txt',
+                    help='The output file that will contain multi-mapping statistics')
+args = parser.parse_args()
 
-multi_mapping_stats("head-mtb-mapping.sam", "mtb-test.txt")
-# cigar_counts_stats("mtb-single-mapping-report-all.sam", "mtb-h37rv-single")
-# cigar_counts_stats("ot-single-mapping-report-all.sam", "ot-ikeda-single")
+if args.input:
+    multi_mapping_stats(args.input, args.output)
+
