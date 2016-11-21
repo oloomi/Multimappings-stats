e9ba6114c228ae31702e7e20f1cc8633802e6835
oloomi <smh.oloomi@gmail.com>
2016-11-21 19:36:11 +1100

    - Set for single-end mapping - Only considers start position

--- mms.py
+++ mms.py
@@ -2,15 +2,19 @@ __author__ = 'soloomi'
 
 import argparse
 from multi_mappings_statistics import *
-from genome_annotation import *
+from read_block_pairs import *
+#from genome_annotation import *
 
 # Getting command line arguments
-parser = argparse.ArgumentParser('Finds multi-mapping statistics')
-parser.add_argument('-i', '--input', required=True, help='The input SAM file that contains read mappings')
-parser.add_argument('-o', '--output', default='output.txt',
-                    help='The output file that will contain multi-mapping statistics')
-args = parser.parse_args()
-
-if args.input:
-    read_cigar_dict = multi_mapping_stats(args.input, args.output)
-    find_annotation(read_cigar_dict, "oti.gb", "genbank")
+# parser = argparse.ArgumentParser('Finds multi-mapping statistics')
+# parser.add_argument('-i', '--input', required=True, help='The input SAM file that contains read mappings')
+# parser.add_argument('-o', '--output', default='output.txt',
+#                     help='The output file that will contain multi-mapping statistics')
+# args = parser.parse_args()
+#
+# if args.input:
+#     read_cigar_dict = multi_mapping_stats(args.input, args.output)
+    # find_annotation(read_cigar_dict, "oti.gb", "genbank")
+    # find_annotation(read_cigar_dict, "oti.gb", "genbank")
+read_pairs('E:\Codes\data\mtb-single-mapping-report-all.sam', 'block-pairs-mtb-single.txt')
+read_pairs('E:\Codes\data\ot-single-mapping-report-all.sam', 'block-pairs-ot-single.txt')
--- read_block_pairs.py
+++ read_block_pairs.py
@@ -35,21 +35,26 @@ def read_pairs(sam_file_path, output_file_path):
         if len(pos_list) > 1:
             for pos in pos_list:
                 # It only belongs to one block eg. [101,199] that is block no. 2
-                block_no = int(pos / 100) + 1
-                if pos % 100 == 1:
-                    read_block_dict[read].append(block_no)
-                # Otherwise, the read maps to 2 adjacent blocks
-                else:
-                    if block_no > 1:
-                        read_block_dict[read].append(block_no - 1)
-                    read_block_dict[read].append(block_no)
+                block_no = pos // 100 + 1
+                read_block_dict[read].append(block_no)
+                # if pos % 100 == 1:
+                #     read_block_dict[read].append(block_no)
+                # # Otherwise, the read maps to 2 adjacent blocks
+                # else:
+                #     if block_no > 1:
+                #         read_block_dict[read].append(block_no - 1)
+                #     read_block_dict[read].append(block_no)
+
+    with open('log-{}'.format(output_file_path), 'w') as log_file:
+        for read, block_list in read_block_dict.items():
+            log_file.write('{}\n'.format(sorted(block_list)))
 
     # We find pairs of blocks which share a read
     block_pair_dict = defaultdict(int)
     for read, block_list in read_block_dict.items():
         block_pairs = combinations(block_list, 2)
         for pair in block_pairs:
-            block_pair_dict[pair] += 1
+            block_pair_dict[tuple(sorted(pair))] += 1
 
     with open(output_file_path, 'w') as block_pair_file:
         for pair_count in sorted(block_pair_dict.items()):
