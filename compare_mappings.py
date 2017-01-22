2d017d59ca0980f7727dc5db42efef09785c570f
oloomi <smh.oloomi@gmail.com>
2017-01-22 23:56:58 +1100

    Function compare_mappings is added.

--- compare_mappings.py
+++ compare_mappings.py
@@ -4,12 +4,12 @@ from collections import defaultdict
 
 
 def compare_mappings(benchmark_sam_file_name, mapping_sam_file_name, output_file_name):
-    # A dictionary like: {read_id: [list of mapping positions]}
-    read_position_dict = defaultdict(list)
+    # A dictionary like: {read_id: [list of mappings CIGAR strings]}
+    read_alignments_dict = defaultdict(list)
 
     not_mapped_reads = 0  # Number of reads that are not mapped to any location
 
-    # Reading the SAM file and creating a dictionary of read_id : mapping-positions
+    # Reading the SAM file and creating a dictionary of read_id : alignment
     with open(mapping_sam_file_name) as sam_file:
         for line in sam_file:
             # Skip header lines
@@ -19,10 +19,44 @@ def compare_mappings(benchmark_sam_file_name, mapping_sam_file_name, output_file
             read_id = fields[0]  # QNAME: Query template NAME
             cigar = fields[5]  # CIGAR string (ie. alignment)
             pos = int(fields[3])  # 1-based leftmost mapping POSition
+            md_z = fields[-2]   # Alignment
             # * means no alignment for a read
             if cigar != "*":
-                # How many times a CIGAR string is seen for each read?
-                # Store the mapping locations for each CIGAR in a list
-                read_position_dict[read_id].append(pos)
+                # Store all alignments of a read
+                read_alignments_dict[read_id].append((pos, cigar, md_z))
             else:
                 not_mapped_reads += 1
+
+    print("Number of reads not mapped:", not_mapped_reads)
+
+    with open(benchmark_sam_file_name) as sam_file:
+        with open(output_file_name, 'w') as out_file:
+            line_num = 0
+            for line in sam_file:
+                # Skip header lines
+                if line[0] == "@":
+                    continue
+
+                fields = line.rstrip().split("\t")
+                read_id = fields[0]  # QNAME: Query template NAME
+                cigar = fields[5]  # CIGAR string (ie. alignment)
+                pos = int(fields[3])  # 1-based leftmost mapping POSition
+
+                # Add pair suffix to read ID
+                line_num += 1
+                if line_num % 2 == 0:
+                    read_id += "/2"
+                else:
+                    read_id += "/1"
+
+                # * means no alignment for a read
+                if cigar != "*":
+                    if read_id in read_alignments_dict and len(read_alignments_dict[read_id]) > 1:
+                        out_file.write("{}\t{}\n".format((pos, cigar), read_alignments_dict[read_id]))
+                    # else:
+                    #     out_file.write("read not found!\n")
+
+
+
+compare_mappings("E:\Codes\data\correct-mapping\mtb.sam",
+                 "E:\Codes\data\correct-mapping\mtb-single-mapping-report-all.sam", "compared-SAM-mtb.txt")
